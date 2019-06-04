0808ec180b30ab181abc7d1ab9d9876350c69ea2
Pablo Hernandez <prh90@yahoo.com>
2019-06-03 20:45:55 -0700

    Adds geolocation commands

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+    return <div>Hi there</div>
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
